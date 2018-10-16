59aab779f72b144086b81e832ac036303e02ccc9
DarioPC <ferraidario@gmail.com>
2018-10-17 00:39:21 +0200

    A19 update

---
+++ Source/NaturalFloors/Building_Composter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using Verse;
+using RimWorld;
+namespace NaturalFloors
+{
+    public class Building_Composter : Building_Storage
+    {
+        private readonly Thing _compost = ThingMaker.MakeThing(ThingDef.Named("freshCompost"));
+        private readonly DamageInfo _damageInfo = new DamageInfo(DamageDefOf.Rotting, 1);
+
+        public override void TickRare()
+        {
+            //List all things in cell
+            var thingsInComposter = Map.thingGrid.ThingsListAt(Position);
+            if (thingsInComposter.Count <= 0)
+            {
+                return;
+            }
+
+            //Loop over things
+            foreach (var thing in thingsInComposter.ToList())
+            {
+                if (thing.def.category != ThingCategory.Item || thing.def.defName == "freshCompost")
+                {
+                    continue;
+                }
+                
+                if (thing.def.defName.Contains("Corpse"))
+                {
+                    if (!(thing is Corpse corpse))
+                    {
+                        throw new InvalidOperationException($"Snable to cast {thing.Label} to a corpse. Something went wrong");
+                    }
+
+                    if (corpse.InnerPawn.def.race.Animal || corpse.InnerPawn.def.race.Humanlike)
+                    {
+                        thing.TakeDamage(_damageInfo);
+                        _compost.stackCount = 25;
+                    }
+                }
+                else
+                {
+                    thing.TakeDamage(_damageInfo);
+                    _compost.stackCount = thing.stackCount;
+                }
+
+                if (thing.HitPoints >= 10)
+                {
+                    continue;
+                }
+
+                thing.Destroy();
+                GenSpawn.Spawn(_compost, Position, Map);
+            }
+        }
+    }
+}
---
+++ Source/NaturalFloors/PlaceWorker_NearWater.cs
@@ -0,0 +1,19 @@
+﻿using Verse;
+namespace NaturalFloors
+{
+    public class PlaceWorker_NearWater : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 location, Rot4 rot, Map map, Thing thingToIgnore = null)
+        {
+            foreach (var current in GenRadial.RadialCellsAround(location, 5f, true))
+            {
+                if (current.GetTerrain(map).IsRiver || current.GetTerrain(map).IsWater || current.GetTerrain(map) == TerrainDef.Named("Ice"))
+                {
+                    return true;
+                }
+            }
+
+            return "MustBeNearWaterSource".Translate();
+        }
+    }
+}
